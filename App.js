2ba89ba6eaaa8a179d8cfbc621a43cc6e6f82def
James Davies <hybridbad90@gmail.com>
2019-08-13 14:40:18 +0100

    test expo build

--- App.js
+++ App.js
@@ -1,27 +1,66 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
+import { API_KEY } from './utils/WeatherAPIKey';
+import Weather from './components/Weather';
 
-class Greeting extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
+export default class App extends React.Component {
+
+    state = {
+      isLoading: true,
+      temperature: 0,
+      weatherCondition: null,
+      error: null
+    };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Error getting weather data'
+        });
+        console.log(error)
+      }
     );
+  }
+
+  fetchWeather(lat = 25, lon = 25) {
+    fetch(
+      `https://samples.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        this.setState({
+          temperature: (json.main.temp - 273.15).toFixed(2),
+          weatherCondition: json.weather[0].main,
+          isLoading: false,
+        })
+        
+      });
+  }
+
+  render() {
+    const { isLoading, weatherCondition, temperature } = this.state;
+
+    return (
+      <View style={styles.container}>
+        {
+          isLoading ? <Text>Fetching the data</Text> : <Weather weather={weatherCondition} temperature={temperature} />
+        }
+      </View>
+    )
   }
 }
 
-export default class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center', top: 50}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
-      </View>
-    );
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
-}
-
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
+});
---
+++ utils/WeatherAPIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = "041ea81f62d3849eef4ad195aee88ebd"
